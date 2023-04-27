9a15ed65d8f593a8ee113d74ac33215de13265d3
Seoras79 <seorasproductions@gmail.com>
2023-04-27 14:58:04 +0100

    middleware created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -22,15 +25,50 @@ app.get('/', async (req, res, next) => {
 // Verifies token with jwt.verify and sets req.user
 // TODO - Create authentication middleware
 
+function authenticate (req, res, next) {
+  const authHeader = req.get('Authorization');
+
+  if (!authHeader) {
+    res.set('WWW-Authenticate', 'Bearer')
+    res.sendStatus(401);
+    return;
+  }
+
+  const [type, token] = authHeader.split(' ');
+
+  if (type.toLowerCase() !== "bearer" || !token) {
+    console.error("invalid token");
+    res.sendStatus(401);
+    return
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (err) {
+    return res.status(401).send({ error: 'Invalid token' });
+  }
+};
+app.use(authenticate);
+
+app.use(setUser);
+
 // POST /register
 // OPTIONAL - takes req.body of {username, password} and creates a new user with the hashed password
 
+app.post('/register', async (req, res, next) => {
+  const newUser = req.body(username, password);
+  const hashedPW = bcrypt.hash(password, process.env.JWT_SECRET)
+})
 // POST /login
 // OPTIONAL - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
 
 // GET /kittens/:id
 // TODO - takes an id and returns the cat with that id
+app.get('/:id', async (req, res, next) => {
 
+})
 // POST /kittens
 // TODO - takes req.body of {name, age, color} and creates a new cat with the given name, age, and color
 
